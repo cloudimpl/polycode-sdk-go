4214a7cdfd474f5a5c4955576a39cf24f4f1761d
cloudimpl <github.bot@cloudimpl.com>
2025-09-10 02:48:42 +0000

    Update from https://github.com/cloudimpl/ByteOS/commit/e4a7f0ac9bf9646a8ffcf08fe533f5e416c3e447

--- validator.go
+++ validator.go
@@ -1,22 +1,5 @@
 package sdk
 
-var currentValidator Validator = DummyValidator{}
-
-func GetValidator() Validator {
-	return currentValidator
-}
-
-func SetValidator(v Validator) {
-	currentValidator = v
-}
-
 type Validator interface {
 	Validate(obj any) error
-}
-
-type DummyValidator struct {
-}
-
-func (v DummyValidator) Validate(obj any) error {
-	return nil
 }
