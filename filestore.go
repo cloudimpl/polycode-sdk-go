1fc2d9a19a4a3202588c01540780825bc0047179
cloudimpl <github.bot@cloudimpl.com>
2025-10-02 10:13:45 +0000

    Update from https://github.com/cloudimpl/byte-os/commit/b10fd296d45a04f59f3bf819161d86b43227f88e

--- filestore.go
+++ filestore.go
@@ -2,15 +2,30 @@ package polycode
 
 import "time"
 
+type FileMetaData struct {
+	Created  time.Time `json:"created"`
+	Modified time.Time `json:"modified"`
+	Size     int64     `json:"size"`
+}
+
+type ReadOnlyFile interface {
+	Parent() ReadOnlyFolder
+	Name() string
+	Path() string
+	Metadata() FileMetaData
+
+	Read() ([]byte, error)
+	Download(localFilePath string) error
+	GetDownloadLink() (string, error)
+}
+
 type File interface {
 	Parent() Folder
 	Name() string
 	Path() string
-	Size() int64
-	Created() time.Time
-	Modified() time.Time
+	Metadata() FileMetaData
 
-	Get() (bool, []byte, error)
+	Read() ([]byte, error)
 	Download(filePath string) error
 	GetDownloadLink() (string, error)
 
@@ -22,6 +37,16 @@ type File interface {
 	Rename(newName string) error
 	MoveTo(dest Folder) error
 	CopyTo(dest Folder) error
+}
+
+type ReadOnlyFolder interface {
+	Parent() ReadOnlyFolder
+	Name() string
+	Path() string
+
+	Folder(name string) ReadOnlyFolder
+	File(name string) ReadOnlyFile
+	List(maxFiles int32, offsetToken *string) ([]ReadOnlyFile, *string, error)
 }
 
 type Folder interface {
